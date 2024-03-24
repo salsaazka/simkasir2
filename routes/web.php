49801fe5bf0bd57def6efbab0581302bbe8b1d75
Yogapratama1109 <yoga110011@gmail.com>
2024-03-24 16:39:23 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -8,58 +8,70 @@ use App\Http\Controllers\SaleController;
 use App\Http\Controllers\DetailSaleController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('pages.dashboard');
+})->name('dashboard');
 
-Route::get('/', [AuthController::class, 'signIn'])->name('signIn');
-Route::get('/', [AuthController::class, 'auth'])->name('auth');
-Route::get('/sign-up', [AuthController::class, 'signUp'])->name('signUp');
-Route::get('/sign-up', [AuthController::class, 'register'])->name('register');
+Route::get('/transaksi', function () {
+    return view('pages.transaksi');
+})->name('transaksi');
 
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/sign-up', function () {
+    return view('auth.signup');
+})->name('signUp');
+
+Route::get('/sign-in', function () {
+    return view('auth.signin');
+})->name('signIn');
+
+// Route::get('/', [AuthController::class, 'signIn'])->name('signIn');
+// Route::get('/', [AuthController::class, 'auth'])->name('auth');
+// Route::get('/sign-up', [AuthController::class, 'signUp'])->name('signUp');
+// Route::get('/sign-up', [AuthController::class, 'register'])->name('register');
+
+// Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 // Buyer
 // Route::middleware('isLogin')->prefix('/buyer')->name('buyer.')->group(function () {
-    
+
 // });
-Route::get('/', [BuyerController::class, 'index'])->name('index');
-Route::get('/create-buyer', [BuyerController::class, 'create'])->name('create');
-Route::post('/create-buyer', [BuyerController::class, 'index'])->name('index');
-Route::get('/edit{id}', [BuyerController::class, 'edit'])->name('edit');
-Route::patch('/update/{id}', [BuyerController::class, 'update'])->name('update');
-Route::delete('/delete/{id}', [BuyerController::class, 'destroy'])->name('destroy');
+// Route::get('/', [BuyerController::class, 'index'])->name('index');
+// Route::get('/create-buyer', [BuyerController::class, 'create'])->name('create');
+// Route::post('/create-buyer', [BuyerController::class, 'index'])->name('index');
+// Route::get('/edit{id}', [BuyerController::class, 'edit'])->name('edit');
+// Route::patch('/update/{id}', [BuyerController::class, 'update'])->name('update');
+// Route::delete('/delete/{id}', [BuyerController::class, 'destroy'])->name('destroy');
 
 // Product
 // Route::middleware('isLogin')->prefix('/product')->name('product.')->group(function () {
-    
+
 // });
-Route::get('/', [ProductController::class, 'index'])->name('index');
-Route::get('/create-product', [ProductController::class, 'create'])->name('create');
-Route::post('/create-product', [ProductController::class, 'index'])->name('index');
-Route::get('/edit{id}', [ProductController::class, 'edit'])->name('edit');
-Route::patch('/update/{id}', [ProductController::class, 'update'])->name('update');
-Route::delete('/delete/{id}', [ProductController::class, 'destroy'])->name('destroy');
+// Route::get('/', [ProductController::class, 'index'])->name('index');
+// Route::get('/create-product', [ProductController::class, 'create'])->name('create');
+// Route::post('/create-product', [ProductController::class, 'index'])->name('index');
+// Route::get('/edit{id}', [ProductController::class, 'edit'])->name('edit');
+// Route::patch('/update/{id}', [ProductController::class, 'update'])->name('update');
+// Route::delete('/delete/{id}', [ProductController::class, 'destroy'])->name('destroy');
 
 // Sale
 // Route::middleware('isLogin')->prefix('/sale')->name('sale.')->group(function () {
-    
+
 // });
-Route::get('/', [SaleController::class, 'index'])->name('index');
-Route::get('/create-sale', [SaleController::class, 'create'])->name('create');
-Route::post('/create-sale', [SaleController::class, 'index'])->name('index');
-Route::get('/edit{id}', [SaleController::class, 'edit'])->name('edit');
-Route::patch('/update/{id}', [SaleController::class, 'update'])->name('update');
-Route::delete('/delete/{id}', [SaleController::class, 'destroy'])->name('destroy');
+// Route::get('/', [SaleController::class, 'index'])->name('index');
+// Route::get('/create-sale', [SaleController::class, 'create'])->name('create');
+// Route::post('/create-sale', [SaleController::class, 'index'])->name('index');
+// Route::get('/edit{id}', [SaleController::class, 'edit'])->name('edit');
+// Route::patch('/update/{id}', [SaleController::class, 'update'])->name('update');
+// Route::delete('/delete/{id}', [SaleController::class, 'destroy'])->name('destroy');
 
 
 // Detail Sale
 // Route::middleware('isLogin')->prefix('/detailsale')->name('detailsale.')->group(function () {
-    
+
 // });
-Route::get('/', [DetailSaleController::class, 'index'])->name('index');
-Route::get('/create-detailsale', [DetailSaleController::class, 'create'])->name('create');
-Route::post('/create-detailsale', [DetailSaleController::class, 'index'])->name('index');
-Route::get('/edit{id}', [DetailSaleController::class, 'edit'])->name('edit');
-Route::patch('/update/{id}', [DetailSaleController::class, 'update'])->name('update');
-Route::delete('/delete/{id}', [DetailSaleController::class, 'destroy'])->name('destroy');
+// Route::get('/', [DetailSaleController::class, 'index'])->name('index');
+// Route::get('/create-detailsale', [DetailSaleController::class, 'create'])->name('create');
+// Route::post('/create-detailsale', [DetailSaleController::class, 'index'])->name('index');
+// Route::get('/edit{id}', [DetailSaleController::class, 'edit'])->name('edit');
+// Route::patch('/update/{id}', [DetailSaleController::class, 'update'])->name('update');
+// Route::delete('/delete/{id}', [DetailSaleController::class, 'destroy'])->name('destroy');
 
